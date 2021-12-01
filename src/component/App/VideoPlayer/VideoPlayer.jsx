ca8e61ec1dd0a3dc351722bc64e683a91d45d3c2
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-12-01 14:33:22 -0500

    Created useEffect that allows any video ID to be input into the embedded video player. (VPP, GD, VG)

--- src/component/App/VideoPlayer/VideoPlayer.jsx
+++ src/component/App/VideoPlayer/VideoPlayer.jsx
@@ -1,34 +1,33 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
 
-class VideoPlayer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            enteredVideoId: {},
-        }
-    }
+// class VideoPlayer extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             enteredVideoId: {},
+//         }
+//     }
     
-    selectedVideo() {
-        let response = "Tch4v0L0GHA";
-        console.log(response);
-        this.setState({
-            enteredVideoId: response
-        })
-    }
+//     selectedVideo() {
+//         let response = "Tch4v0L0GHA";
+//         console.log(response);
+//         this.setState({
+//             enteredVideoId: response
+//         })
+//     }
     
-    render() {
-        this.selectedVideo();
-        return(
-        <div>
-            <iframe id="ytplayer" type="text/html" width="640" height="360"
-                src={`https://www.youtube.com/embed/${this.state.enteredVideoId}?autoplay=1&origin=http://example.com`}
-                frameBorder="0">
-            </iframe>
-            <h1>The title and Description will go here.</h1>
-        </div>)
-    }
+//     render() {
+//        return(
+//         <div>
+//             <iframe id="ytplayer" type="text/html" width="640" height="360"
+//                 src={`https://www.youtube.com/embed/${this.state.enteredVideoId}?autoplay=1&origin=http://example.com`}
+//                 frameBorder="0">
+//             </iframe>
+//             <h1>The title and Description will go here.</h1>
+//         </div>)
+//     }
 
-}
+// }
 // const VideoPlayer = (props) => {
     
 //     return (<div>
@@ -41,5 +40,31 @@ class VideoPlayer extends Component {
 
 //             </div>)}
     
+function VideoPlayer() {
+    const[selectedVideo, setSelectedVideo] = useState(null);
+    useEffect(() => {
+        getSelectedVideo();
+    }, [])
+
+    async function getSelectedVideo(){
+            const response = "GbtF3oA7NMM";
+            console.log(response);
+            setSelectedVideo(response);
+    }
+
+    return (
+        <div>
+            <div>
+                {selectedVideo &&
+                <iframe id="ytplayer" type="text/html" width="640" height="360"
+                   src={`https://www.youtube.com/embed/${selectedVideo}?autoplay=1&origin=http://example.com`} frameborder="0">
+                </iframe>
+                }
+                <h1>The title and Description will go here.</h1>
+            </div>
+        </div>
+    )
+}
+
 
 export default VideoPlayer;
