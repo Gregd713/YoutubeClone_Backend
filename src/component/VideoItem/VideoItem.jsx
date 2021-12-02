e23960fa8f174b4dec7f4b228864400ab92f06f9
Gregory Delgado <gregd92694@yahoo.com>
2021-12-02 13:27:12 -0600

    was able to move text into table format for suggested videos.

--- src/component/VideoItem/VideoItem.jsx
+++ src/component/VideoItem/VideoItem.jsx
@@ -2,13 +2,20 @@ import React from "react";
 
 const VideoItem = ({ video }) => {
     return(
-        <div>
-            <ul>
-                <h4>{video.snippet.title}</h4>
-                <img  src={video.snippet.thumbnails.default.url} />
-                <h4>{video.snippet.description}</h4>
-            </ul>       
-        </div>
+        // <div>
+        //     <ul>
+        //         <h4>{video.snippet.title}</h4>
+        //         <img  src={video.snippet.thumbnails.default.url} />
+        //         <h4>{video.snippet.description}</h4>
+        //     </ul>       
+        // </div>
+        <table>
+        <tr>
+            <td><img  src={video.snippet.thumbnails.default.url} /></td>
+            <h4>{video.snippet.title}</h4>
+            <h4>{video.snippet.description}</h4>
+        </tr>
+        </table>
     )
     
 }
