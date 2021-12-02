94a3f74ed3cfeb2bf169f0958ab077b2a59f2ed7
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-12-02 13:25:22 -0500

    Created title/description display for video player. (VPP)

--- src/component/VideoPlayer/VideoPlayer.jsx
+++ src/component/VideoPlayer/VideoPlayer.jsx
@@ -1,13 +1,18 @@
 import React, {Component} from "react";
 
 
-const VideoPlayer = ({ video }) => {
+const VideoPlayer = ({ video, videoObject }) => {
     const videoSrc = `https://www.youtube.com/embed/${video}?autoplay=1&origin=http://example.com`
     return (
-        <iframe id="ytplayer" type="text/html" width="640" height="360"
-            src={ videoSrc }
-            frameborder="0">
-        </iframe>)}
+        <div>
+            <iframe id="ytplayer" type="text/html" width="640" height="360"
+                src={ videoSrc }
+                frameborder="0">
+            </iframe>)}
+            <h3>{videoObject}</h3>
+        </div>
+    )
+}
     
 
 export default VideoPlayer;
